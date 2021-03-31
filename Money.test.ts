39d1d168b82ec4a2220d2058f7edead7bddeeb10
Pierre414 <p.poetter27@gmail.com>
2021-03-31 11:26:05 +0200

    Nur noch eine Klasse für alles

--- Money.test.ts
+++ Money.test.ts
@@ -1,5 +1,5 @@
-import { Dollar } from "./Dollar";
-import { Franc } from "./Franc"
+// import { Dollar } from "./Dollar";
+// import { Franc } from "./Franc"
 import { Money } from "./Money";
 
 
@@ -10,29 +10,37 @@ const { not } = require('expect');
 
 
 test('5 Dollars time 2 is 10', () => {
-    const fiveDollars = new Dollar(5,'Dollar');
-    let result = fiveDollars.times(2)
-    expect(result).toBe((10));
+    const fiveDollars = Money.dollar(5,null);
+    fiveDollars.dollartimes(2)
+    expect(fiveDollars.getamount()).toBe((10));
 });
 
 test('5 Dollars time 3 is 15', () => {
-    const fiveDollars = new Dollar(5,'Dollar');
-    let result = fiveDollars.times(3)
-    expect(result).toBe((15));
+    const fiveDollars = Money.dollar(5,'Dollar');
+    fiveDollars.dollartimes(3)
+    expect(fiveDollars.getamount()).toBe((15));
 });
 test('5 Dollar equal 5 Dollar', () => {
-    expect(new Dollar(5,'Dollar')).toStrictEqual(new Dollar(5,'Dollar'))
+    expect(Money.dollar(5,'Dollar')).toStrictEqual(Money.dollar(5,'Dollar'))
 });
 
 test('5 Franc time 3 is 15', () => {
-    const fiveFranc = new Franc(5,'Franc')
-    let result = fiveFranc.times(3)
-    expect(result).toBe((15));
+    const fiveFranc = Money.franc(5,'Franc')
+    fiveFranc.franctimes(3)
+    expect(fiveFranc.getamount()).toBe((15));
 });
 
 test('5 Dollar not equal 5 Franc', () => {
-    const fiveDollars=new Dollar(5,'Dollar');
-    const fiveFranc=new Franc(5,'Franc');
-    expect(fiveDollars).not.toEqual(fiveFranc)
-});                                                                                     
+    const fiveDollars=Money.dollar(5,'Dollar');
+    const fiveFranc=Money.franc(5,'Franc');
+    expect(fiveDollars).not.toBe(fiveFranc)
+});              
+
+test('simple Addition',()=>{
+    let sum:Money=Money.dollar(5,null).plus(Money.dollar(5,null));
+    let sum2:Money=Money.dollar(10,null);
+    expect(sum.getamount()).toBe(sum2.getamount());
+})
+
+
  
--- Money.ts
+++ Money.ts
@@ -1,26 +1,41 @@
+// import { Dollar } from "./Dollar";
+// import { Franc } from "./Franc";
+
 export class Money {
+  protected amount: number;
+  protected currency: String;
 
-    protected amount: number
-    protected currency:String
-
-    constructor(amount: number,currency:String) {
-        this.amount = amount
-    }
-    public times(multiplier: number) {
-        this.amount *= multiplier;
-        return this.amount;
-    }
-    public getamount(): number {
-        return this.amount  
-    }
-    public getcurrency():String{
-        return this.currency;
-    }
-    public equals(obj: Money): boolean {
-        return this.currency==obj.getcurrency() && this.amount==obj.getamount();
-        
-     }                          
-
-
- 
+  constructor(amount: number, currency: String) {
+    this.amount = amount;
+  }
+  public dollartimes(multiplier: number) {
+    this.amount *= multiplier;
+    this.currency="USD";
+    return this.currency
+    return this.amount;
+  }
+  public franctimes(multiplier: number) {
+    this.amount *= multiplier;
+    this.currency="CHF";
+    return this.currency
+    return this.amount;
+  }
+  public getamount(): number {
+    return this.amount;
+  }
+  public getcurrency(): String {
+    return this.currency;
+  }
+  static dollar(amount: number, currency: String): Money {
+    return new Money(amount, currency);
+  }
+  static franc(amount: number, currency: String): Money {
+    return new Money(amount, currency);
+  }
+  public equals(obj: Money): boolean {
+    return this.currency == obj.getcurrency() && this.amount == obj.getamount();
+  }
+  public plus(addend:Money):Money{
+      return new Money(this.amount+addend.amount,this.currency);
+  }
 }
